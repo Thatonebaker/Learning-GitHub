fd5d504c3b26f749b92ead890dec9d6eae97fedf
Josh Baker <J.cbaker@hotmail.com>
2021-12-20 22:12:00 -0500

    Initial Commit

---
+++ M3L2 Learning GitHub/M3L2_Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2_Learning_GitHubApp.swift
+//  M3L2 Learning GitHub
+//
+//  Created by Macbook on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct M3L2_Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
